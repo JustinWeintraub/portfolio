2e3408d2458e217299f45b31e80a7cc6adc0e979
Yoshterra1 <44651405+Yoshterra1@users.noreply.github.com>
2023-08-18 17:06:59 -0400

    new version of portfolio

--- src/main.js
+++ src/main.js
@@ -1,12 +1,9 @@
 import Vue from "vue";
-import VueHead from 'vue-head'
 
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 //Vue.config.productionTip = false;
 Vue.config.silent = true;
-
-Vue.use(VueHead)
 
 new Vue({ router, store, render: (h) => h(App) }).$mount("#app");
